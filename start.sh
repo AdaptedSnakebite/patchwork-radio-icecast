2a18af897faf6376ea917754a056ab08b2afa8ef
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-19 19:24:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# Do the sed replacement as root, then switch to nobody user
 sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /etc/icecast/icecast.xml
-exec icecast -c /etc/icecast/icecast.xml
+# Switch to nobody user and start icecast
+exec su-exec nobody icecast -c /etc/icecast/icecast.xml
