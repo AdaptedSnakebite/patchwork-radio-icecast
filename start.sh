823c843649cd1a994b26ce96d1ee11e89a88902d
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-19 23:17:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
+
 echo "Railway assigned PORT: $PORT"
 
 cp /etc/icecast/icecast.xml /tmp/icecast.xml
 
-# Replace the port in the config file with the Railway port
-sed -i "s/<port>8080<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
-
-# Replace passwords with env variables (or default)
-sed -i "s/<source-password>.*<\/source-password>/<source-password>${SOURCE_PASS:-hackme}<\/source-password>/" /tmp/icecast.xml
-sed -i "s/<admin-password>.*<\/admin-password>/<admin-password>${ADMIN_PASS:-admin123}<\/admin-password>/" /tmp/icecast.xml
+# Replace the port 8000 with the Railway assigned port dynamically
+sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
 
 echo "Starting Icecast on port $PORT (not running as root)"
+
 icecast -c /tmp/icecast.xml -b
