52f3b3b7ef8adb732cc7ff02dade0c7bf197809a
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-19 23:04:49 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
 echo "Railway assigned PORT: $PORT"
 
+# Copy config file to temp folder so we can edit it
 cp /etc/icecast/icecast.xml /tmp/icecast.xml
-sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
+
+# Replace default port with Railway assigned port
+sed -i "s/<port>8080<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
 
 echo "Starting Icecast on port $PORT (not running as root)"
-icecast -c /tmp/icecast.xml -b
+exec icecast -c /tmp/icecast.xml -b -n nobody
