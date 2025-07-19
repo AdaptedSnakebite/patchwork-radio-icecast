25b79a285584fdd63f11761165bea74fdb65c089
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-19 23:18:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ echo "Railway assigned PORT: $PORT"
 
 cp /etc/icecast/icecast.xml /tmp/icecast.xml
 
-# Replace the port 8000 with the Railway assigned port dynamically
 sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
 
 echo "Starting Icecast on port $PORT (not running as root)"
 
-icecast -c /tmp/icecast.xml -b
+icecast -c /tmp/icecast.xml
