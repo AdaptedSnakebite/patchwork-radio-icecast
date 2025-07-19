03ca640126749cc8a7b871d91fb8e6b0b8f4e396
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-19 19:31:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 echo "Railway assigned PORT: $PORT"
 cp /etc/icecast/icecast.xml /tmp/icecast.xml
 sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /tmp/icecast.xml
-echo "Starting Icecast on port $PORT"
-exec icecast -c /tmp/icecast.xml
+chown nobody:nogroup /tmp/icecast.xml
+echo "Starting Icecast on port $PORT as nobody user"
+exec su-exec nobody icecast -c /tmp/icecast.xml
