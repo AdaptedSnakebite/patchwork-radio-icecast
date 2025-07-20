f0ff52848aa8648f079242d417502ef9718e174a
AdaptedSnakebite <59810662+AdaptedSnakebite@users.noreply.github.com>
2025-07-20 21:54:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-# Create icecast user and group
-addgroup -S icecast && adduser -S -G icecast icecast
-
-# Create logging directory and fix permissions
-mkdir -p /logs
-chown -R icecast:icecast /logs
-
-# Start Icecast as non-root
-exec su-exec icecast icecast2 -c /etc/icecast.xml
+# Ensure Icecast is started by non-root user
+exec su-exec nobody icecast -c /etc/icecast.xml
